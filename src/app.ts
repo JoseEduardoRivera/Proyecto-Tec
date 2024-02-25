de338c3547a0bebd3eb4f562f9f19f74d511bd69
JoseEduardoRivera <pepecarrerapepe@gmail.com>
2024-02-24 20:07:39 -0600

    Preparacion inicial

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { envs } from './config/envs';
+import { AppRoutes } from './presentation/routes';
+import { Server } from './presentation/server';
+
+//Funcion asincrona autoinvocada
+(async()=> {
+  main();
+})();
+
+
+function main() {
+
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
